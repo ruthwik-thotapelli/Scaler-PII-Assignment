181b2ec63efbf0299ed1359e45afb94ec4ed631d
Ruthwik Thotapelli <ruthwikthotapelli8@gmail.com>
2026-08-13 10:44:30 +0530

    feat: build global entity cache map to ensure cross-doc mapping consistency

--- redact_pii.py
+++ redact_pii.py
@@ -4,3 +4,9 @@ from faker import Faker
 
 faker = Faker()
 analyzer = AnalyzerEngine()
+ENTITY_MAP = {}
+
+def get_fake_value(entity_type, real_value):
+    if real_value in ENTITY_MAP:
+        return ENTITY_MAP[real_value]
+    # Generator logic here...
