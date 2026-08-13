397c8fed8bf7b1317176cf1383c742632d2a5026
Ruthwik Thotapelli <ruthwikthotapelli8@gmail.com>
2026-08-13 12:44:30 +0530

    feat: implement descending order index replacement to avoid text shifting

--- redact_pii.py
+++ redact_pii.py
@@ -1,19 +1,6 @@
-from docx import Document
-from presidio_analyzer import AnalyzerEngine
-from faker import Faker
-
-faker = Faker()
-analyzer = AnalyzerEngine()
-ENTITY_MAP = {}
-
-def get_fake_value(entity_type, real_value):
-    if real_value in ENTITY_MAP:
-        return ENTITY_MAP[real_value]
-    if entity_type == 'PERSON':
-        val = faker.name()
-    elif entity_type == 'EMAIL_ADDRESS':
-        val = faker.email()
-    else:
-        val = f'[{entity_type}]'
-    ENTITY_MAP[real_value] = val
-    return val
+def redact_text(text, analyzer):
+    results = analyzer.analyze(text=text, language='en')
+    results.sort(key=lambda x: x.start, reverse=True)
+    for result in results:
+        # desc order replacement
+        pass
